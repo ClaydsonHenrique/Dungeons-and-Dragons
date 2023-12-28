dc62ae6140f59e5dccf6dcee24ba58a3c000c05a
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-12-28 16:58:07 -0300

    finalizando requisito 5

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,24 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetypes';
+
+export default class Ranger extends Archetype {
+  private _energy: EnergyType;
+  private static count = 0;
+  constructor(_name: string) {
+    super(_name);
+    this._energy = 'stamina';
+    Ranger.count += 1;
+  }
+
+  public get name() {
+    return this._name;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+
+  static createdArchetypeInstances() {
+    return Ranger.count;
+  }
+}
